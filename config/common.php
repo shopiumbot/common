999181d4d7d625c158f0abf98aa730bf8067baf8
Andrew <andrew.panix@gmail.com>
2020-04-20 20:51:26 +0300

    init

--- config/common.php
+++ config/common.php
@@ -60,11 +60,18 @@ $config = [
         ],
         'i18n' => [
             'translations' => [
-                'app*' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/messages',
+                    'fileMap' => [
+                        'default' => 'default.php',
+                    ],
+                ],
+                'app/*' => [
                     'class' => 'yii\i18n\PhpMessageSource',
                     'basePath' => '@vendor/panix/engine/messages',
                     'fileMap' => [
-                        'app' => 'app.php',
+                        'app/default' => 'default.php',
                         'app/admin' => 'admin.php',
                         'app/month' => 'month.php',
                         'app/error' => 'error.php',
