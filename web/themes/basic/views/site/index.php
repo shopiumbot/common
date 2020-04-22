c99934d487fdc9a51544338d6f5384b53b701152
Andrew <andrew.panix@gmail.com>
2020-04-22 18:55:30 +0300

    init

--- web/themes/basic/views/site/index.php
+++ web/themes/basic/views/site/index.php
@@ -8,7 +8,7 @@ $config = Yii::$app->settings->get('telegram');
 <div>
     <div class="jumbotron text-center">
         <h1>Добро пожаловать!</h1>
-        <?php if (!isset($config->bot_name) && !empty($config->bot_name)) { ?>
+        <?php if (isset($config->bot_name) && !empty($config->bot_name)) { ?>
             <p class="lead"><i class="icon-telegram-outline"></i>
                 <strong><?= Html::a('@' . $config->bot_name, 'tg://@' . $config->bot_name, ['class' => 'text-danger']); ?></strong>
             </p>
