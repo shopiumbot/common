dd753a918d804f0c72b88fc6c46be169e32a6a4c
Andrew <andrew.panix@gmail.com>
2020-06-09 14:30:40 +0300

    init

--- web/themes/dashboard/views/layouts/partials/_navbar.php
+++ web/themes/dashboard/views/layouts/partials/_navbar.php
@@ -1,5 +1,12 @@
 <?php
 use panix\engine\Html;
+use yii\helpers\Url;
+use panix\engine\CMS;
+
+$api = new \shopium\mod\telegram\components\Api;
+$bot = \shopium\mod\telegram\models\User::find()->where(['id'=>$api->getBotId()])->one();
+$botImage = $bot->getPhoto();
+
 ?>
 <ul class="navbar-nav float-left mr-auto">
     <!-- <li class="nav-item d-none d-md-block">
@@ -15,7 +22,7 @@ use panix\engine\Html;
             <div class="d-flex align-items-center">
                 <i class="icon-search font-20 mr-1"></i>
                 <div class="ml-1 d-none d-sm-block">
-                    <span>Search</span>
+                    <span>Поиск</span>
                 </div>
             </div>
         </a>
@@ -28,7 +35,11 @@ use panix\engine\Html;
     </li>
 </ul>
 <ul class="navbar-nav float-right">
+    <?php
 
+    $orders = \shopium\mod\cart\models\Order::find()->where('status_id')->all();
+    if($orders){
+    ?>
     <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#"
            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -36,36 +47,32 @@ use panix\engine\Html;
             <span class="badge badge-pill badge-success"><?= Yii::$app->getModule('cart')->count['num']; ?></span>
         </a>
         <div class="dropdown-menu dropdown-menu-right mailbox animated bounceInDown" aria-labelledby="2">
-                                <span class="with-arrow">
-                                    <span class="bg-danger"></span>
-                                </span>
+            <span class="with-arrow"><span class="bg-danger"></span></span>
             <ul class="list-style-none">
                 <li>
                     <div class="drop-title text-white bg-danger">
-                        <h4 class="m-b-0 m-t-5"><?= Yii::$app->getModule('cart')->count['num']; ?> Заказов</h4>
+                        <h4 class=""><?= ucfirst(Yii::t('cart/default','ORDERS_COUNTER',Yii::$app->getModule('cart')->count['num']));?></h4>
                         <span class="font-light"></span>
                     </div>
                 </li>
                 <li>
                     <div class="message-center message-body">
-                        <!-- Message -->
                         <?php
-                        $s = new \shopium\mod\telegram\components\Api;
-                        $orders = \shopium\mod\cart\models\Order::find()->where('status_id')->all();
+
                         foreach ($orders as $order){
 
             //                \panix\engine\CMS::dump($order->user->getPhoto());
                         ?>
-                            <a href="javascript:void(0)" class="message-item">
+                            <a href="<?= Url::to(['/admin/cart/default/update','id'=>$order->id]);?>" class="message-item">
                                                 <span class="user-img">
-                                                    <img src="<?= $order->user->getPhoto(); ?>" alt="user"
+                                                    <img src="<?= $order->user->getPhoto(); ?>" alt="<?= $order->firstname; ?> <?= $order->lastname; ?>"
                                                          class="rounded-circle">
                                                     <span class="profile-status online pull-right"></span>
                                                 </span>
                                 <div class="mail-contnet">
-                                    <h5 class="message-title"><?= $order->firstname; ?><?= $order->lastname; ?></h5>
-                                    <span class="mail-desc"><?= $order->productsCount;?> товара</span>
-                                    <span class="time"><?= $order->total_price; ?> грн</span>
+                                    <h5 class="message-title"><?= $order->firstname; ?> <?= $order->lastname; ?></h5>
+                                    <span class="mail-desc"><?= Yii::t('shop/default','PRODUCTS_COUNTER',$order->productsCount);?></span>
+                                    <span class="time"><?= Yii::$app->currency->number_format($order->total_price); ?> грн</span>
                                 </div>
                             </a>
                         <?php } ?>
@@ -79,7 +86,7 @@ use panix\engine\Html;
             </ul>
         </div>
     </li>
-
+<?php } ?>
     <li class="nav-item dropdown border-right  d-none">
         <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown"
            aria-haspopup="true" aria-expanded="false">
@@ -93,7 +100,7 @@ use panix\engine\Html;
             <ul class="list-style-none">
                 <li>
                     <div class="drop-title bg-primary text-white">
-                        <h4 class="m-b-0 m-t-5">4 New</h4>
+                        <h4 class="">4 New</h4>
                         <span class="font-light">Notifications</span>
                     </div>
                 </li>
@@ -154,16 +161,11 @@ use panix\engine\Html;
             </ul>
         </div>
     </li>
-    <!-- ============================================================== -->
-    <!-- End Comment -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- User profile and search -->
-    <!-- ============================================================== -->
+
     <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle pro-pic" href=""
            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <img src="<?= $asset->baseUrl; ?>/images/2.jpg" alt="user" class="rounded-circle"
+            <img src="<?= $botImage; ?>" alt="<?= $bot->username; ?>" class="rounded-circle"
                  width="40">
             <span class="m-l-5 font-medium d-none d-sm-inline-block"><?= Yii::$app->user->getDisplayName(); ?> <i
                         class="icon-arrow-down"></i></span>
@@ -174,25 +176,24 @@ use panix\engine\Html;
                                 </span>
             <div class="d-flex no-block align-items-center p-2 bg-primary text-white mb-3">
                 <div class="">
-                    <img src="<?= $asset->baseUrl; ?>/images/2.jpg" alt="user" class="rounded-circle"
+                    <img src="<?= $botImage; ?>" alt="<?= $bot->username; ?>" class="rounded-circle"
                          width="60">
                 </div>
                 <div class="ml-2">
                     <h5 class="mb-0"><?= Yii::$app->user->getDisplayName(); ?></h5>
-                    <p class="mb-0"><a href="/cdn-cgi/l/email-protection"
-                                       class="__cf_email__">[email&#160;protected]</a>
+                    <p class="mb-0"><?= $bot->username; ?></a>
                     </p>
                 </div>
             </div>
             <div class="profile-dis scrollable">
-                <a class="dropdown-item" href="/user/profile">
+                <a class="dropdown-item d-none" href="/profile">
                     <i class="icon-user-outline mr-1 ml-1"></i> Аккаунт</a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="/logout">
                     <i class="icon-logout mr-1 ml-1"></i> Выход</a>
                 <div class="dropdown-divider"></div>
             </div>
-            <div class="pl-3 p-2">
+            <div class="pl-3 p-2 d-none">
                 <a href="javascript:void(0)" class="btn btn-sm btn-success btn-rounded">View Profile</a>
             </div>
         </div>
