4be1c5fb4b11058d04317a7add075118ea12f1b2
Andrew <andrew.panix@gmail.com>
2020-08-10 16:02:17 +0300

    init

---
+++ migrations/m999999_999999_update_1.php
@@ -0,0 +1,24 @@
+<?php
+
+use panix\engine\db\Migration;
+use panix\engine\components\Settings;
+
+class m999999_999999_update_1 extends Migration
+{
+
+
+    public function up()
+    {
+
+
+        $settings[] = ['app', 'availability_hide', false];
+        $settings[] = ['csv', 'pagenum', 250];
+        $settings[] = ['csv', 'indent_row', 1];
+        $settings[] = ['csv', 'indent_column', 1];
+        $settings[] = ['csv', 'ignore_columns', NULL];
+
+        $this->batchInsert(Settings::tableName(), ['category', 'param', 'value'], $settings);
+    }
+
+
+}
